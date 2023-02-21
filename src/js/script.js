777646670440ac7a9dfc6aa976053cce4e7d057b
melkoff <vasamelko@gmail.com>
2023-02-21 17:49:20 +0200

    fixed burger-menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,24 @@ const humb = document.querySelector('.burger');
 const humbBody = document.querySelector('.burger__menu');
 const header = document.querySelector('.header');
 const menuOpen = document.querySelector('html');
-
+const humbLink = document.querySelector('.burger__menu');
 
 humb.addEventListener('click', humbHandler);
 
-function humbHandler(e) {
-  e.preventDefault();
+function humbHandler(burgerOn) {
+  burgerOn.preventDefault();
   humbBody.classList.toggle("open");
+  header.classList.toggle('active');
   menuOpen.classList.toggle("menu__open");
   humb.classList.toggle('active');
-  header.classList.toggle('active');
-  // window.scrollTo(0, 0);
+  humbLink.classList.toggle('active');
 }
+$('.burger__menu a').on('click', function () {
+  $('.burger__menu').toggleClass('open');
+  $('.burger').toggleClass('active');
+  $('html').toggleClass('menu__active');
+})
+// ============================================= //
 
 
 //Music effect
@@ -24,6 +30,7 @@ $(".title__event_card")
   .mouseenter(function () {
     beepOne.play();
   });
+// ============================================= //
 
 
 //Scroll indicator
@@ -36,3 +43,4 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+// ============================================= //
